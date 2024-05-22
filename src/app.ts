8ebf83af5dd269dcc8d696c5c0774af0400b36af
arju10 <arjuarju71@gmail.com>
2024-05-22 20:25:28 +0600

    settings .vscode changes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import routes from './app/routes'
 const app: Application = express()
 const port = 3000
 
@@ -8,6 +9,9 @@ app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Application Routes
+app.use('/api/v1/', routes)
 
 // Testing
 app.get('/', (req: Request, res: Response) => {
