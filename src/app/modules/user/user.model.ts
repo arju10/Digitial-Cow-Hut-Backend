49440d471f45369ae1fa28dc77d316154150f13c
arju10 <arjuarju71@gmail.com>
2024-05-22 20:52:16 +0600

    Create User Model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,54 @@
+import { Schema, VirtualType, model } from 'mongoose'
+import { IUser, UserModel } from './user.interface'
+
+const userSchema = new Schema<IUser, UserModel>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    name: {
+      required: true,
+      type: {
+        firstName: {
+          type: String,
+          required: true,
+        },
+        lastName: {
+          type: String,
+          required: true,
+        },
+      },
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    budget: {
+      type: Number,
+    },
+    income: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+)
+
+export const User = model<IUser, UserModel>('User', userSchema)
