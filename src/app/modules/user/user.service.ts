09a99d03d7bbbbeb52ea8316afb012039f39a2ee
arju10 <arjuarju71@gmail.com>
2024-05-23 09:45:44 +0600

    Setup catch async

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,13 +1,8 @@
-import ApiError from '../../../errors/ApiError'
 import { IUser } from './user.interface'
 import { User } from './user.model'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   const createdUser = await User.create(user)
-
-  if (!createdUser) {
-    throw new ApiError(400, 'Failed to create User')
-  }
 
   return createdUser
 }
--- src/errors/handleZodError.ts
+++ src/errors/handleZodError.ts
@@ -1,4 +1,4 @@
-import { ZodError, ZodIssue } from 'zod' // Imported ZodError and ZodIssue
+import { ZodError, ZodIssue } from 'zod'
 import { IGenericErrorResponse } from '../interface/common'
 import { IGenericErrorMessage } from '../interface/error'
 
